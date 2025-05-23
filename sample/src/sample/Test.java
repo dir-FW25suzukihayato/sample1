d5ac9de16fdafeb24b838685dc7fcf1e5c896c44
HayatoSuzuki(鈴木颯人) <hayato.suzuki.k620@dirbato.co.jp>
2025-05-23 15:53:09 +0900

    最初のコミット

---
+++ sample/src/sample/Test.java
@@ -0,0 +1,22 @@
+package sample;
+
+public class Test {
+    
+    //mainメソッドはプログラムの最初に動作します
+    public static void main(String[] args) {
+        /* コメントアウト
+         system.out.println("Kiramex");
+         system.out.println("Java");
+         */
+         System.out.println("学習を始めましょう!");
+         
+    }
+
+        
+        
+        
+        
+
+    }
+
+
